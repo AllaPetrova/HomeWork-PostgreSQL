4d56c50f10a05d22805a13c16d68b49630867442
AllaPetrova <petrova.allapet@gmail.com>
2025-05-03 17:57:25 +0300

    Создание БД.sql

--- Создание БД.sql
+++ Создание БД.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS genre (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL
+    name VARCHAR(60) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS executor (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS GenreExecutor (
     id SERIAL PRIMARY KEY,
@@ -16,20 +15,19 @@ CREATE TABLE IF NOT EXISTS GenreExecutor (
     CONSTRAINT unique_genre_executor UNIQUE (genre_id, executor_id) 
 );
 
+CREATE TABLE IF NOT EXISTS albums (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    year_of_release INT CHECK (year_of_release >= 1900 AND year_of_release <= EXTRACT(YEAR FROM CURRENT_DATE))
+);
 
 CREATE TABLE IF NOT EXISTS tracks (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    duration INT NOT NULL,  
-    albums_id INT NOT NULL REFERENCES albums(id)
+    duration INTERVAL NOT NULL CHECK (duration > INTERVAL '0 seconds' AND duration <= INTERVAL '1 hour'),
+    albums_id INT NOT NULL REFERENCES albums(id),
+    CONSTRAINT unique_track_album UNIQUE (name, albums_id)
 );
-
-CREATE TABLE IF NOT EXISTS albums (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    year_of_release INT
-);
-
 
 CREATE TABLE IF NOT EXISTS ExecutorAlbums (
     id SERIAL PRIMARY KEY,
@@ -38,17 +36,15 @@ CREATE TABLE IF NOT EXISTS ExecutorAlbums (
     CONSTRAINT unique_executor_album UNIQUE (executor_id, albums_id)
 );
 
-
 CREATE TABLE IF NOT EXISTS collection (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60) NOT NULL,
-    year_of_release INT NOT NULL
+    year_of_release INT NOT NULL CHECK (year_of_release >= 1900 AND year_of_release <= EXTRACT(YEAR FROM CURRENT_DATE))
 );
-
 
 CREATE TABLE IF NOT EXISTS TracksCollection (
     id SERIAL PRIMARY KEY,
-    tracks_ID INT NOT NULL REFERENCES tracks(id),
+    tracks_id INT NOT NULL REFERENCES tracks(id),
     collection_id INT NOT NULL REFERENCES collection(id),
     CONSTRAINT unique_track_collection UNIQUE (tracks_id, collection_id)
 );
